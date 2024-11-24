9fd918d8edb47ffe1aa90acffd7184af805ce83b
Vishnu S Babu <vishnu@Vishnus-MacBook-Air-3.local>
2024-11-24 14:54:02 +0000

    Editing scatterplot code for labels

--- Scatterplot.R
+++ Scatterplot.R
@@ -2,8 +2,10 @@ library(readr)
 insurance <- read_csv("insurance.csv")
 x <- insurance$age
 y <- insurance$charges
-plot(x,y,main = "Age vs Charges",xlab = "Age of the individual", ylab = "Personal Medical Health Insurance Charges", pch = 19, frame = T, col ="red")
+plot(x,y,main = "Age vs Charges",xlab = "Age of an Individual", ylab = "Personal Medical Health Insurance Charges", pch = 19, frame = T, col ="red")
 model <- lm(y ~ x, data = insurance)
-abline(model, col = "blue")
+abline(model,col = "blue")
+
+
 
 
