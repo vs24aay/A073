6b6d6b0723b640e87aa513fa77cbae04fad4ec2f
Vishnu S Babu <vishnu@Vishnus-MacBook-Air-3.local>
2024-11-24 14:21:43 +0000

    Amending the naming of X and Y axis

--- Scatterplot.R
+++ Scatterplot.R
@@ -2,7 +2,8 @@ library(readr)
 insurance <- read_csv("insurance.csv")
 x <- insurance$age
 y <- insurance$charges
-plot(x,y,main = "Age vs Charges",xlab = "Age", ylab = "Charges", pch = 19, frame = T, col ="red")
+plot(x,y,main = "Age vs Charges",xlab = "Age of the individual", ylab = "Personal Medical Health Insurance Charges", pch = 19, frame = T, col ="red")
 model <- lm(y ~ x, data = insurance)
 abline(model, col = "blue")
+
 
